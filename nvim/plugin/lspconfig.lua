a7c96ae487014fcad750e2def581c98ab2a4f4cf
Charles Moscofian <charlesmoscofian@hotmail.com>
2022-06-10 18:40:47 -0300

    Bump nvim plugins

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -4,15 +4,15 @@ local config = require("cmoscofian.lsp").config
 lspconfig.clangd.setup {
     capabilities = config.capabilities,
     on_attach = config.on_attach,
-    cmd = {"clangd", "--background-index","--suggest-missing-includes"},
-    filetypes = {"c", "cc", "cpp", "h", "hpp", "objc", "objcpp"},
+    cmd = { "clangd", "--background-index", "--suggest-missing-includes" },
+    filetypes = { "c", "cc", "cpp", "h", "hpp", "objc", "objcpp" },
     single_file_support = true,
 }
 
 lspconfig.gopls.setup {
     capabilities = config.capabilities,
     on_attach = config.on_attach,
-    cmd = {"gopls", "serve"},
+    cmd = { "gopls", "serve" },
     settings = {
         gopls = {
             analyses = {
@@ -44,15 +44,15 @@ lspconfig.tsserver.setup {
 lspconfig.sumneko_lua.setup {
     capabilities = config.capabilities,
     on_attach = config.on_attach,
-    cmd = {"lua-language-server"},
+    cmd = { "lua-language-server" },
     settings = {
         Lua = {
             completion = {
                 keywordSnippet = "Disable",
             },
             diagnostics = {
-                globals = {"vim", "use"},
-                disable = {"lowercase-global"}
+                globals = { "vim", "use" },
+                disable = { "lowercase-global" }
             },
             runtime = {
                 version = "LuaJIT",
