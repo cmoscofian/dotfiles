ce707119021702709928a81b1d42b124a9a9404d
Charles Moscofian <charlesmoscofian@hotmail.com>
2022-09-03 11:19:37 -0300

    Configure dap and bump dependencies
    Customize DAP for a better debugging experience and bump all nvim
    dependencies.

--- nvim/plugin/dap.lua
+++ nvim/plugin/dap.lua
@@ -1,11 +1,10 @@
 local dap = require("dap")
 local ui = require("dapui")
-local conf = require("cmoscofian.dap")
+local config = require("cmoscofian.dap")
 
-ui.setup {}
+dap.adapters = config.adapters
+dap.configurations = config.configurations
 
-dap.adapters = conf.adapters
-dap.configurations = conf.configurations
 dap.listeners.after.event_initialized["dapui_config"] = function()
     ui.open()
 end
@@ -16,17 +15,62 @@ dap.listeners.before.event_exited["dapui_config"] = function()
     ui.close()
 end
 
-
-vim.fn.sign_define("DapBreakpoint", { texthl = "Decorator" })
-vim.fn.sign_define("DapBreakpointCondition", { texthl = "Special" })
-vim.fn.sign_define("DapStopped", { texthl = "SpecialChar", linehl = "DiffText" })
-vim.fn.sign_define("DapBreakpointRejected", { texthl = "DiagnosticError" })
-
 local opts = { silent = true }
-vim.keymap.set("n", "<leader>d", dap.toggle_breakpoint, opts)
-vim.keymap.set("n", "<leader>D", function() dap.set_breakpoint(vim.fn.input("Condition: ")) end, opts)
+vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, opts)
+vim.keymap.set("n", "<leader>dB", function() dap.set_breakpoint(vim.fn.input("Condition: ")) end, opts)
+vim.keymap.set("n", "<leader>dc", dap.clear_breakpoints, opts)
+vim.keymap.set("n", "<leader>dr", dap.run_to_cursor, opts)
 vim.keymap.set("n", "<F5>", dap.continue, opts)
 vim.keymap.set("n", "<F10>", dap.step_over, opts)
 vim.keymap.set("n", "<F11>", dap.step_into, opts)
 vim.keymap.set("n", "<F12>", dap.step_out, opts)
-vim.keymap.set("n", "<leader>tu", ui.toggle, opts)
+vim.keymap.set("n", "<leader>dt", ui.toggle, opts)
+
+ui.setup {
+    layouts = {
+        {
+            elements = {
+                "stacks",
+                "breakpoints",
+                "watches",
+                "scopes",
+            },
+            size = 40,
+            position = "left",
+        },
+        {
+            elements = {
+                "repl",
+            },
+            size = 0.3,
+            position = "bottom",
+        },
+    },
+    windows = {
+        indent = 2,
+    },
+}
+
+vim.fn.sign_define {
+    {
+        name = "DapBreakpoint",
+        text = "●",
+        texthl = "DapBreakpoint"
+    },
+    {
+        name = "DapBreakpointCondition",
+        text = "●",
+        texthl = "DapBreakpointCondition"
+    },
+    {
+        name = "DapBreakpointRejected",
+        text = "●",
+        texthl = "DapBreakpointRejected",
+        linehl = "DapBreakpointRejectedLine"
+    },
+    {
+        name = "DapStopped",
+        texthl = "DapStopped",
+        linehl = "DapStoppedLine"
+    },
+}
