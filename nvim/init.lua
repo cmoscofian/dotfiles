233434488d1d8d43a4bc578f1ab1a9eda9deed74
Charles Moscofian <charlesmoscofian@hotmail.com>
2023-05-05 15:45:10 +0200

    feat(env): changes to system environment variables
    Set EDITOR and VISUAL environment variables to use neovim instead of
    vim, also call vim default configuration from nvim using the VIMDIR env
    variable instead of setting the vim folder manually from the DOTDIR.
    
    Work configurations for git should be set inside of GITDIR/work
    
    Also, bump nvim plugins.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
-vim.opt.runtimepath:append("$DOTDIR/vim")
-vim.opt.packpath:append("$DOTDIR/vim")
-vim.cmd("source $DOTDIR/vim/vimrc")
+vim.opt.runtimepath:append("$VIMDIR")
+vim.opt.packpath:append("$VIMDIR")
+vim.cmd("source $VIMDIR/vimrc")
 
 vim.opt.showmode = false
 vim.opt.laststatus = 3
