740f5baee2cc28288168c35d68e42ca3c80f61e3
Charles Moscofian <charlesmoscofian@hotmail.com>
2025-02-20 17:14:29 +0100

    feat(deps): remove lualine
    Signed-off-by: Charles Moscofian <charlesmoscofian@hotmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,8 +2,6 @@ vim.opt.runtimepath:append("$VIMDIR")
 vim.opt.packpath:append("$VIMDIR")
 vim.cmd("source $VIMDIR/vimrc")
 
-vim.opt.showmode = false
 vim.opt.laststatus = 3
-vim.cmd("colorscheme nibblemod")
 
 require("cmoscofian.globals")
